3f93b0b1f7ee7fa928405f8c327aee0775592304
Alex <alex.pober.95@gmail.com>
2024-07-22 16:08:35 -0700

    height of main div isn't working on iphone, so removed dhv

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col h-dvh justify-center items-center p-8 min-[390px]:gap-6 gap-4">
+    <main className="flex flex-col justify-center items-center p-8 min-[390px]:gap-6 gap-4">
       <Image alt="logo" src={Logo} width={300} height={300} className="drop-shadow-lg mb-4 w-40 min-[390px]:w-60"/>
       <div className="w-min mt-[-30px] text-center leading-[1.9rem] min-[390px]:leading-[2.5rem] tracking-tight text-4xl	min-[390px]:text-5xl font-extrabold text-vip-1">
         DRIVER PROTECTION
@@ -16,28 +16,28 @@ export default function Home() {
         className="w-full max-w-screen-sm"
       >
         {" "}
-        <div className="text-xl min-[390px]:text-3xl text-vip-5 bg-vip-2 font-bold outline outline-4 p-3 text-center rounded-full">
+        <div className="text-xl min-[391px]:text-3xl text-vip-5 bg-vip-2 font-bold outline outline-4 p-3 text-center rounded-full">
           Tickets
         </div>
       </Link>
 
-      <div className="text-xl min-[390px]:text-3xl text-vip-5 bg-vip-2 font-bold outline outline-4 p-3 w-22 w-full max-w-screen-sm text-center rounded-full">
+      <div className="text-xl min-[391px]:text-3xl text-vip-5 bg-vip-2 font-bold outline outline-4 p-3 w-22 w-full max-w-screen-sm text-center rounded-full">
         Accidents
       </div>
 
-      <div className="text-xl min-[390px]:text-3xl text-vip-5 bg-vip-2 font-bold outline outline-4 p-3 w-22 w-full max-w-screen-sm text-center rounded-full">
+      <div className="text-xl min-[391px]:text-3xl text-vip-5 bg-vip-2 font-bold outline outline-4 p-3 w-22 w-full max-w-screen-sm text-center rounded-full">
         DUI
       </div>
 
-      <div className="text-xl min-[390px]:text-3xl text-vip-5 bg-vip-2 font-bold outline outline-4 p-3 w-22 w-full max-w-screen-sm text-center rounded-full">
+      <div className="text-xl min-[391px]:text-3xl text-vip-5 bg-vip-2 font-bold outline outline-4 p-3 w-22 w-full max-w-screen-sm text-center rounded-full">
         Towing
       </div>
 
-      <div className="text-xl min-[390px]:text-3xl text-vip-5 bg-vip-2 font-bold outline outline-4 p-3 w-full max-w-screen-sm text-center rounded-full">
+      <div className="text-xl min-[391px]:text-3xl text-vip-5 bg-vip-2 font-bold outline outline-4 p-3 w-full max-w-screen-sm text-center rounded-full">
         Car Rental / Car Rental
       </div>
 
-      <div className="text-xl min-[390px]:text-3xl text-vip-5 bg-vip-2 font-bold outline outline-4 p-3 w-full max-w-screen-sm text-center rounded-full">
+      <div className="text-xl min-[391px]:text-3xl text-vip-5 bg-vip-2 font-bold outline outline-4 p-3 w-full max-w-screen-sm text-center rounded-full">
         Body Shop / Lemon
       </div>
 
